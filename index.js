e4db1c23b84f5221945921249dad22941665ea4d
Soumajit Dutta <ak509667as@gmail.com>
2025-07-08 11:08:57 +0530

    name Function Changed

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const name = "soum";
 const dob = "16-08-05"
 
 function sayName(){
-    console.log("My Name is "+name);
+    console.log(`My Name is ${name}`);
 }
 
-<<<<<<< HEAD
 function newfn(){
     console.log("Hey i am a new function");
 }
 
-=======
 function isAbove18(){
     return age>= 18;
 }
->>>>>>> parent of d9aebe6 (18 function)
